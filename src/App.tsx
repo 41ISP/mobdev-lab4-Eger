0b53b683d44eaf8319e0ee7269950792101fddf8
RomanEger <88767017+RomanEger@users.noreply.github.com>
2025-01-18 07:58:29 +0000

    add geo api

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,117 @@
-import {useEffect, useState} from "react";
-import {Alert, Box, IconButton, Typography} from "@mui/joy";
+import { useEffect, useState } from "react";
+import { Alert, Box, IconButton, Input, Typography } from "@mui/joy";
 import ReportIcon from '@mui/icons-material/Report';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import OpenMeteoAPI from "./shared/api/api.ts";
-import {DailyDto, Temperature, WeatherForecast} from "./shared/api/api.dto.ts";
-import {IWeatherForecastRdo} from "./shared/api/api.rdo.ts";
+import { OpenMeteoAPI, YandexGeoAPI } from "./shared/api/api.ts";
+import { DailyDto, Temperature, WeatherForecast } from "./shared/api/api.dto.ts";
+import { IWeatherForecastRdo } from "./shared/api/api.rdo.ts";
 
 function App() {
-	const [hasLocation, setHasLocation] = useState(false);
+	const [address, setAddress] = useState('')
+
+	const [latitude, setLatitude] = useState<number>()
+	const [longitude, setLongitude] = useState<number>()
+
+	// const [hasLocation, setHasLocation] = useState(false);
 	const [alert, setAlert] = useState(false);
 
 	const [weatherForecast, setWeatherForecast] = useState<IWeatherForecastRdo>();
 
-	useEffect(() => {
-		navigator.permissions.query({
-			name: 'geolocation'
-		}).then(function (result) {
-			if (result.state === 'denied') {
-				setAlert(true);
-				return
-			} else {
-				setAlert(false);
-			}
-		});
+	// useEffect(() => {
+	// 	navigator.permissions.query({
+	// 		name: 'geolocation'
+	// 	}).then(function (result) {
+	// 		if (result.state === 'denied') {
+	// 			setAlert(true);
+	// 			return
+	// 		} else {
+	// 			setAlert(false);
+	// 		}
+	// 	});
 
-		if (hasLocation) return
+	// 	if (hasLocation) return
 
-		navigator.geolocation.getCurrentPosition(
-			async (pos: GeolocationPosition) => {
-				setHasLocation(true);
-				console.log(pos.coords.latitude, pos.coords.longitude);
+	// 	navigator.geolocation.getCurrentPosition(
+	// 		async (pos: GeolocationPosition) => {
+	// 			setHasLocation(true);
+	// 			console.log(pos.coords.latitude, pos.coords.longitude);
 
-				const result = await OpenMeteoAPI.getWeatherForecast(
-					new WeatherForecast(
-						pos.coords.latitude,
-						pos.coords.longitude,
-						Temperature.allTemperature,
-						DailyDto.sunriseAndSunset
-					)
-				)
-				if (result?.data) {
-					console.log(result.data)
-					setWeatherForecast(result.data)
-				}
-			})
-	}, [hasLocation]);
+	// 			const result = await OpenMeteoAPI.getWeatherForecast(
+	// 				new WeatherForecast(
+	// 					pos.coords.latitude,
+	// 					pos.coords.longitude,
+	// 					Temperature.allTemperature,
+	// 					DailyDto.sunriseAndSunset
+	// 				)
+	// 			)
+	// 			if (result?.data) {
+	// 				console.log(result.data)
+	// 				setWeatherForecast(result.data)
+	// 			}
+	// 		})
+	// }, [hasLocation]);
 
 	useEffect(() => {
 		console.log(weatherForecast);
 	}, [weatherForecast]);
+
+	const getCity = async () => {
+		if (!address)
+			return
+		const response = await YandexGeoAPI.getCoordinates(address)
+		console.log('getCity data')
+
+		console.log(response)
+
+		const result = response?.data?.response?.GeoObjectCollection?.featureMember[0].GeoObject.Point.pos.split(' ')
+		return result
+	}
+
+	const search = async () => {
+		console.log('search start');
+		console.log(address);
+
+		console.log(`latitude: ${latitude}`);
+		console.log(`longitude: ${longitude}`)
+		if (!latitude || !longitude) return
+		
+		const result = await OpenMeteoAPI.getWeatherForecast(
+			new WeatherForecast(
+				latitude,
+				longitude,
+				Temperature.allTemperature,
+				DailyDto.sunriseAndSunset
+			)
+		)
+
+		console.log('search data');
+		console.log(result);		
+
+		if (result?.data) {
+			console.log('search result data');
+
+			console.log(result.data)
+			setWeatherForecast(result.data)
+		}
+	}
+
+	useEffect(() => {
+		const timeoutId = setTimeout(async () => {
+			const pos = await getCity()
+
+			if (!pos) return
+
+			setLatitude(Number(pos[1]))
+			setLongitude(Number(pos[0]))
+		}, 500);
+
+		return () => clearTimeout(timeoutId);
+	}, [address])
+
+	useEffect(() => {
+		const s = async () => search()
+		s()
+	}, [longitude, latitude])
 
 	return (
 		<>
@@ -61,34 +124,39 @@ function App() {
 			>
 				{
 					alert &&
-                    <Alert
-                        sx={{maxWidth: 350}}
-                        variant={"soft"}
-                        color={"danger"}
-                        startDecorator={<ReportIcon/>}
-                        endDecorator={
+					<Alert
+						sx={{ maxWidth: 350 }}
+						variant={"soft"}
+						color={"danger"}
+						startDecorator={<ReportIcon />}
+						endDecorator={
 							<IconButton onClick={() => setAlert(false)} variant="soft" color={'danger'}>
-								<CloseRoundedIcon/>
+								<CloseRoundedIcon />
 							</IconButton>
 						}
-                    >
-                        <div>
-                            <Typography level="body-md">
-                                Ошибка
-                            </Typography>
-                            <Typography level="body-sm" color={"danger"}>
-                                Необходимо разрешение на определение местоположения
-                            </Typography>
-                        </div>
-                    </Alert>
+					>
+						<div>
+							<Typography level="body-md">
+								Ошибка
+							</Typography>
+							<Typography level="body-sm" color={"danger"}>
+								Необходимо разрешение на определение местоположения
+							</Typography>
+						</div>
+					</Alert>
+				}
+				<Input
+					placeholder="Населенный пункт"
+					onChange={(e) => { setAddress(e.target.value) }}>
+					{address}
+				</Input>
+				{
+					weatherForecast &&
+					<>
+						
+					</>
 				}
 			</Box>
-			{
-				weatherForecast &&
-				<>
-					
-				</>
-			}
 		</>
 	)
 }
--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,20 +1,23 @@
 import axios, {AxiosResponse} from "axios";
 import {WeatherForecast} from "./api.dto.ts";
-import {IWeatherForecastRdo} from "./api.rdo.ts";
+import {IWeatherForecastRdo, IResponse} from "./api.rdo.ts";
 
-const BaseURL = 'https://api.open-meteo.com/v1/forecast'
+const OpenMeteoURL = 'https://api.open-meteo.com/v1/forecast'
+
+const Yandex_API_KEY = import.meta.env.VITE_API_KEY
+const YandexGeoURL = 'https://geocode-maps.yandex.ru/1.x/'
 
 /**
  * Represents Open-Metio API
  * https://api.open-meteo.com/#
  */
-const OpenMetioAPIAxios = axios.create({baseURL: BaseURL})
+const OpenMeteoAPIAxios = axios.create({baseURL: OpenMeteoURL})
 
-const OpenMetioAPI = {
+export const OpenMeteoAPI = {
 	getWeatherForecast: async (weatherForecast: WeatherForecast):Promise<AxiosResponse<IWeatherForecastRdo> | undefined> => {
 		try {
-			return await OpenMetioAPIAxios.get<IWeatherForecastRdo>(
-				`${BaseURL}`,
+			return await OpenMeteoAPIAxios.get<IWeatherForecastRdo>(
+				`${OpenMeteoURL}`,
 				{
 					params: {
 						latitude: weatherForecast.latitude,
@@ -31,4 +34,26 @@ const OpenMetioAPI = {
 	}
 }
 
-export default OpenMetioAPI
+const YandexGeoAPIAxios = axios.create({baseURL: YandexGeoURL})
+
+export const YandexGeoAPI = {
+	
+	getCoordinates: async(address: string) => {
+		try {
+			console.log('in api call '+address);
+			
+			return await YandexGeoAPIAxios.get<IResponse>(
+				`${YandexGeoURL}`,
+				{
+					params: {
+						apikey: Yandex_API_KEY?.toString(),
+						geocode: address,
+						format: 'json'
+					}
+				}
+			)
+		} catch (err) {
+			console.error(err)
+		}
+	}
+}
